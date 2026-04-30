caa405a19c4f19604a30a174c79bb2da35bf4cc6
oliverjeno <O.jennings-2023@hull.ac.uk>
2026-04-30 04:48:14 +0100

    OS_Resource_Allocation_Lab

---
+++ OS_Resource_Allocation_Lab task 4/bankers algorithm/bankers algorithm/Program.cs
@@ -0,0 +1,111 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Number of processes: ");
+        int processes = int.Parse(Console.ReadLine());
+
+        Console.Write("Number of resource types: ");
+        int resources = int.Parse(Console.ReadLine());
+
+        int[,] allocation = new int[processes, resources];
+        int[,] max = new int[processes, resources];
+        int[,] need = new int[processes, resources];
+        int[] available = new int[resources];
+        bool[] finished = new bool[processes];
+        int[] safeSequence = new int[processes];
+
+        
+        Console.WriteLine("Enter allocation matrix (with a space between):");
+        for (int i = 0; i < processes; i++)
+        {
+            Console.Write("P" + (i + 1) + ": ");
+            string[] input = Console.ReadLine().Split(' ');
+
+            for (int j = 0; j < resources; j++)
+            {
+                allocation[i, j] = int.Parse(input[j]);
+            }
+        }
+
+       
+        Console.WriteLine("Enter maximum demand matrix (with a space between):");
+        for (int i = 0; i < processes; i++)
+        {
+            Console.Write("P" + (i + 1) + ": ");
+            string[] input = Console.ReadLine().Split(' ');
+
+            for (int j = 0; j < resources; j++)
+            {
+                max[i, j] = int.Parse(input[j]);
+                need[i, j] = max[i, j] - allocation[i, j];
+            }
+        }
+
+       
+        Console.WriteLine("Enter available resources (with a space between):");
+        string[] availInput = Console.ReadLine().Split(' ');
+        for (int j = 0; j < resources; j++)
+        {
+            available[j] = int.Parse(availInput[j]);
+        }
+
+        int count = 0;
+
+        while (count < processes)
+        {
+            bool foundProcess = false;
+
+            for (int i = 0; i < processes; i++)
+            {
+                if (!finished[i])
+                {
+                    bool canRun = true;
+
+                    for (int j = 0; j < resources; j++)
+                    {
+                        if (need[i, j] > available[j])
+                        {
+                            canRun = false;
+                            break;
+                        }
+                    }
+
+                    if (canRun)
+                    {
+                        for (int j = 0; j < resources; j++)
+                        {
+                            available[j] += allocation[i, j];
+                        }
+
+                        safeSequence[count] = i;
+                        finished[i] = true;
+                        count++;
+                        foundProcess = true;
+                    }
+                }
+            }
+
+            if (!foundProcess)
+            {
+                Console.WriteLine("System is not in a safe state.");
+                return;
+            }
+        }
+
+        Console.Write("Safe Sequence: ");
+        for (int i = 0; i < processes; i++)
+        {
+            Console.Write("P" + (safeSequence[i] + 1));
+            if (i < processes - 1)
+                Console.Write(" -> ");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("System is in a safe state ");
+        Console.Read(); 
+    }
+}
---
+++ lab 8 code task/lab 8 code task/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+class TLBEntry
+{
+    public int VPN;
+    public int PPN;
+}
+class TLB
+{
+    private List<TLBEntry> entries = new List<TLBEntry>(); private const int capacity = 4; // TLB capacity 
+    public int? Lookup(int vpn)
+    {
+        foreach (var entry in entries)
+        {
+            if
+    (entry.VPN == vpn) return entry.PPN;
+        }
+        return null; // TLB miss 
+    }
+    public void Insert(int vpn, int ppn)
+    {
+        if (entries.Count >= capacity)
+            entries.RemoveAt(0); entries.Add(new TLBEntry { VPN = vpn, PPN = ppn });
+    }
+}
+//1.Simulate TLB hits and misses. 
+//2. Track the hit ratio after a sequence of address translations
+
+class Program
+{
+    static void Main()
+    {
+        TLB tlb = new TLB(); 
+        int[] virtualAddresses = { 0x0000, 0x1000, 0x2000, 0x3000, 0x0000, 0x1000, 0x4000 }; // Sample virtual addresses
+        int hits = 0; 
+        int total = virtualAddresses.Length; 
+        foreach (var va in virtualAddresses)
+        {
+            int vpn = va >> 12; 
+            int? ppn = tlb.Lookup(vpn);
+            if (ppn.HasValue)
+            {
+                hits++;
+                Console.WriteLine($"TLB Hit: VPN {vpn} and PPN {ppn.Value}");
+            }
+            else
+            {
+                Console.WriteLine($"TLB Miss: VPN {vpn}");
+                tlb.Insert(vpn, vpn); 
+            }
+        }
+        double hitRatio = (double)hits / total;
+        Console.WriteLine($"Hit Ratio: {hitRatio:P2}");
+    }
+}
