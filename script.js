6c06ffda30309cc34fc7f770f57b59e8825de28a
oliverjeno <O.jennings-2023@hull.ac.uk>
2026-07-23 00:09:22 +0100

    added nav bar, added dark mode, fixed website layout, added work to website porfolio from word doc, added more detail to writing

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const myName = document.querySelector(".name");
 console.log(myName);
+
 const description = document.querySelector(".description");
 console.log(description);
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
